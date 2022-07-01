097be41991538555e791653c1183b61b6ede98ad
Joao <jean_janduy@yahoo.com.br>
2022-07-01 07:52:46 -0300

    Achei melhor colocar script em arquivo separado, e lembrei do defer

---
+++ js/mycodejs.js
@@ -0,0 +1,7 @@
+$(".instagram-phone").click(function() {
+    $(this).fadeOut(100);
+    $(".instagram-continue").css("width","100%");
+})
+$(".get-the-app").click(function() {
+    $(this).fadeOut(100);
+})
